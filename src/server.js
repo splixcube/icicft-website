bee1bc95ef5d46157e87525ab138986654ceca1b
Bhavya Agrawal <agrawalbhavya943@gmail.com>
2026-04-04 16:44:17 +0530

    port change

--- src/server.js
+++ src/server.js
@@ -5,7 +5,7 @@ import { fileURLToPath } from "url";
 import fs from "fs";
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
